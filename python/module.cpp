18db2fe77c4c8205382b219c1ee7406da4ebe2e1
Oleg V Obolenskiy <highpower@mail.ru>
2011-12-19 19:33:28 +0400

    fixed python bindings

--- python/module.cpp
+++ python/module.cpp
@@ -1,12 +1,43 @@
 #include "acsetup.hpp"
 
 #include <boost/python.hpp>
+#include "uatraits/details/detector_impl.hpp"
 
 namespace uatraits { namespace python {
 
+namespace py = boost::python;
+
+class python_detector {
+
+public:
+	python_detector(char const *file);
+	py::dict detect(py::str str) const;
+
+private:
+	typedef details::detector_impl<py::dict> impl_type;
+	shared_ptr<impl_type> impl_;
+};
+
+python_detector::python_detector(char const *file)
+{
+	using namespace details;
+	resource<xmlDocPtr, xml_doc_traits> doc(xmlParseFile(file));
+	xml_throw_unless(doc);
+	impl_.reset(new impl_type(doc.get()));
+}
+
+py::dict
+python_detector::detect(py::str str) const {
+	py::dict result;
+	char const *data = py::extract<char const*>(str);
+	std::string::size_type size = static_cast<std::string::size_type>(py::len(str));
+	impl_->detect(data, data + size, result);
+	return result;
+}
+
 BOOST_PYTHON_MODULE(uatraits) {
-
-
+	py::class_<python_detector> cl("detector", py::init<char const*>());
+	cl.def("detect", &python_detector::detect);
 }
 
 }} // namespaces
