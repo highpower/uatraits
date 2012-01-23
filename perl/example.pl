bdadbeec43fa9ac2cc8239a6fb4d8207ed4db6b9
Oleg V Obolenskiy <highpower@mail.ru>
2012-01-24 02:33:58 +0400

    perl example

--- perl/example.pl
+++ perl/example.pl
@@ -6,6 +6,9 @@ use uatraits;
 eval {
 	my $traits = uatraits->new("browser.xml");
 	my $result = $traits->detect("");
+	for (my ($key, $value) = each(%$result)) {
+		print "$key = $value\n";
+	}
 };
 
 if ($@) {
