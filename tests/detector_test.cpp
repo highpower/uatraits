76a2764970e376e5ec92e0f55b789b6e10cf795e
ponomarev <ponomarev@yandex-team.ru>
2012-01-25 17:34:06 +0300

    added test for Safari (mobile)

--- tests/detector_test.cpp
+++ tests/detector_test.cpp
@@ -23,6 +23,22 @@ BOOST_AUTO_TEST_CASE(test_explorer) {
 	}
 }
 
+BOOST_AUTO_TEST_CASE(test_mobile_safari) {
+
+	std::string line =
+		"Mozilla/5.0 (Linux; U; Android 2.3; ru-ru; Desire_A8181 Build/FRF91) "
+		"AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+
+	detector det(getenv("DATAFILE"));
+	std::map<std::string, std::string> result = det.detect(line);
+
+	BOOST_CHECK_EQUAL(result.find("BrowserEngine")->second, "WebKit");
+	BOOST_CHECK_EQUAL(result.find("BrowserName")->second, "MobileSafari");
+	BOOST_CHECK_EQUAL(result.find("OSFamily")->second, "Android");
+	BOOST_CHECK_EQUAL(result.find("isMobile")->second, "true");
+	BOOST_CHECK_EQUAL(result.find("isTouch")->second, "true");
+}
+
 BOOST_AUTO_TEST_SUITE_END();
 
 }} // namespaces
