9c80706b374357e20e221f5267afe13046f82da5
Oleg V Obolenskiy <highpower@mail.ru>
2012-01-27 16:21:14 +0400

    adding missed files

---
+++ tests/string_match_test.cpp
@@ -0,0 +1,18 @@
+#include "acsetup.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+
+#include "test_utils.hpp"
+#include "uatraits/details/string_match.hpp"
+
+namespace uatraits { namespace tests {
+
+BOOST_AUTO_TEST_SUITE(string_match_test)
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_match, Sequence, test_param_list) {
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+}} // namespaces
