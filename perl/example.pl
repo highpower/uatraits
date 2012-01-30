08b1bb299f418ed5f45f3b974b5f1bd3b3d910ed
ponomarev <ponomarev@yandex-team.ru>
2012-01-30 13:55:28 +0300

    fixed example

--- perl/example.pl
+++ perl/example.pl
@@ -2,13 +2,12 @@
 
 use strict;
 use uatraits;
+use Data::Dumper;
 
 eval {
-	my $traits = uatraits->new("browser.xml");
-	my $result = $traits->detect("");
-	for (my ($key, $value) = each(%$result)) {
-		print "$key = $value\n";
-	}
+	my $traits = uatraits->new("../data/browser.xml");
+	my $result = $traits->detect("UA Opera/9.7 (WindowsMobile; PPC; Opera Mobi/35267; U; ru; Presto/2.1.1)");
+	print Dumper($result);
 };
 
 if ($@) {
