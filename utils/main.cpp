11739eb251d1ef044c5e04f28b3570a062265e4d
ponomarev <ponomarev@yandex-team.ru>
2012-02-02 10:14:20 +0300

    small refactoring ;)

--- utils/main.cpp
+++ utils/main.cpp
@@ -9,7 +9,28 @@
 
 int
 main(int argc, char *argv[]) {
+
+	if (argc != 2) {
+		std::cout << "Usage: check-uatraits <browsers.xml> < <user-agents>" << std::endl;
+		return EXIT_SUCCESS;
+	}
 	try {
+		uatraits::detector engine(argv[1]);
+	        std::string line;
+		while (std::getline(std::cin, line)) {
+			if (line.empty()) {
+				continue;
+			}
+
+			std::cout << line << std::endl;
+
+			typedef uatraits::detector::result_type result_type;
+			result_type res;
+			engine.detect(line, res);
+			for (result_type::const_iterator it = res.begin(), end = res.end(); it != end; ++it) {
+				std::cout << " " << it->first << ": " << it->second << std::endl;
+			}
+		}
 		return EXIT_SUCCESS;
 	}
 	catch (std::exception const &e) {
